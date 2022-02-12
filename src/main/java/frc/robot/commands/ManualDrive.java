e19790d9590c91256cd86fe0d2f6c8334fa05b0a
Natashaxcode <antonich085@lebanon.k12.or.us>
2022-02-11 19:39:45 -0800

    Gave New Commands

--- src/main/java/frc/robot/commands/ManualDrive.java
+++ src/main/java/frc/robot/commands/ManualDrive.java
@@ -1,8 +1,10 @@
 
 package frc.robot.commands;
 
+//import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.CommandBase;
-
+//import frc.robot.Robot;
+import frc.robot.RobotContainer;
 import frc.robot.subsystems.DriveSystem;
 
 /**
@@ -11,27 +13,31 @@ import frc.robot.subsystems.DriveSystem;
 public class ManualDrive extends CommandBase {
 
     private final DriveSystem m_driveSystem;
+ 
 
     public ManualDrive(DriveSystem subsystem) {
-
         m_driveSystem = subsystem;
         addRequirements(m_driveSystem);
-
     }
 
     // Called when the command is initially scheduled.
     @Override
     public void initialize() {
+
     }
 
     // Called every time the scheduler runs while the command is scheduled.
     @Override
     public void execute() {
+        double leftSpeed = RobotContainer.getInstance().getdriverController().getLeftY();
+        double rightSpeed = RobotContainer.getInstance().getdriverController().getRightY();
+        m_driveSystem.move(leftSpeed, rightSpeed);
     }
 
     // Called once the command ends or is interrupted.
     @Override
     public void end(boolean interrupted) {
+        
     }
 
     // Returns true when the command should end.
@@ -43,6 +49,5 @@ public class ManualDrive extends CommandBase {
     @Override
     public boolean runsWhenDisabled() {
         return false;
-
     }
 }
--- src/main/java/frc/robot/subsystems/DriveSystem.java
+++ src/main/java/frc/robot/subsystems/DriveSystem.java
@@ -59,5 +59,7 @@ public class DriveSystem extends SubsystemBase {
         // This method will be called once per scheduler run when in simulation
 
     }
-
+    public void move(double leftSpeed, double rightSpeed) {
+        differentialDrive.tankDrive(leftSpeed, rightSpeed, true);
+    }
 }
