6be0b1bbf36393acb428828c5ff3d09b612c227b
CedAnimator <ced601ced@gmail.com>
2022-02-14 19:12:30 -0800

    changed base drivespeed

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -16,9 +16,9 @@ public class Constants {
         public static final int kLeftFrontPort = 1;
         public static final int kRightFrontPort = 23;
         //Drive motor speeds
-        public static final float kBoostSpeed = 0.85f;
+        public static final float kBoostSpeed = 0.75f;
         public static final float kMaxDriveSpeed = 0.85f;
-        public static final float kBaseDriveSpeed = 0.15f;
+        public static final float kBaseDriveSpeed = 0.25f;
         //Motor varibles
         public static final double PID_P = 0.00001;
         public static final double PID_I = 0.000005;
