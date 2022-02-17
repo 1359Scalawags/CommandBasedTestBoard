a4eb811d70be448847e711cecdc63148057e73a0
CedAnimator <ced601ced@gmail.com>
2022-02-16 18:43:25 -0800

    Added basic vision code

---
+++ src/main/java/frc/robot/subsystems/VisionSystem.java
@@ -0,0 +1,40 @@
+
+package frc.robot.subsystems;
+
+import frc.robot.commands.*;
+import edu.wpi.first.wpilibj.livewindow.LiveWindow;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
+
+public class VisionSystem extends SubsystemBase {
+
+    NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
+    NetworkTableEntry tx = table.getEntry("tx");
+    NetworkTableEntry ty = table.getEntry("ty");
+    NetworkTableEntry ta = table.getEntry("ta");
+
+    public VisionSystem() {
+    }
+
+    @Override
+    public void periodic() {
+        // read values periodically
+        double x = tx.getDouble(0.0);
+        double y = ty.getDouble(0.0);
+        double area = ta.getDouble(0.0);
+
+        // post to smart dashboard periodically
+        SmartDashboard.putNumber("LimelightX", x);
+        SmartDashboard.putNumber("LimelightY", y);
+        SmartDashboard.putNumber("LimelightArea", area);
+    }
+
+    @Override
+    public void simulationPeriodic() {
+
+    }
+
+}
